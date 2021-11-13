2144cd765be114018c394dbcbab7df24a9d00cd6
Salem Eid <salem.eid@gmail.com>
2021-11-13 17:16:11 +0300

    Added Particles , changes to cats

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import Random from '../components/Random';
+import Particles from "react-tsparticles";
 import './App.css';
 
 const App = () => {
@@ -26,6 +27,52 @@ const App = () => {
       <h1>Loading</h1> :
       (
         <div className='tc'>
+          <Particles
+      id="tsparticles"
+      // init={particlesInit}
+      // loaded={particlesLoaded}
+      options={{
+        fpsLimit: 60,
+         particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.3,
+            width: 1,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "star",
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={onSearchChange}/>
           <Random className='f1'/>
